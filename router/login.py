b22b618e7832a4573682873dc6621dba481118b3
hyh8414 <hyh8414@gmail.com>
2023-03-31 01:38:07 +0900

    token response data update

--- router/login.py
+++ router/login.py
@@ -24,7 +24,7 @@ def 로그인(user: User):
     if user.userId == "admin" and user.userPwd == "1234":
         access_token = security.create_access_token(subject=user.userId)
         reflash_token = security.create_reflash_token(subject=user.userId)
-        return Token({"access_token": access_token, "reflash_token": reflash_token, "token_type": "bearer"})
+        return Token(access_token=access_token, reflash_token=reflash_token, token_type="bearer")
     else:
         raise HTTPException(
             status_code=401, detail="Incorrect username or password")
